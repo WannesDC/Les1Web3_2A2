f25ab4490d44f99685094f6010a906fad99aaa6e
Wannes De Craene <wannes.decraene.y0550@student.hogent.be>
2018-09-28 12:16:58 +0200

    Added ability to create a bank account

---
+++ Banking2A2/Models/Domain/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2A2.Models.Domain
+{
+	class BankAccount
+	{
+		//Ctrl+K+S to surround with region!
+		//private string _accountnumber;
+		#region field
+		private decimal _balance;
+		#endregion
+
+		//public const decimal WithdrawCost = 0.10M;
+		//public static readonly decimal _withdrawCost = 0.10M;
+		#region properties
+		public decimal Balance
+		{
+			get
+			{
+				return _balance;
+			}
+			private set
+			{
+				if (value < 0)
+					throw new ArgumentException("Balance cannot be negative.");
+
+				_balance = value;
+			}
+		}
+
+		public string AccountNumber { get; private set; }
+		#endregion
+
+		#region methods
+
+		
+		public void Deposit(decimal amount)
+		{
+			if(amount < 0)
+				throw new ArgumentException("Deposits cannot be negative.");
+			Balance += amount;
+		}
+
+		public void Withdraw(decimal amount)
+		{
+			if (amount < 0)
+				throw new ArgumentException("Withdrawals cannot be negative.");
+			//Ctrl+D: Copy line below
+
+			Balance -= amount;
+		}
+		#endregion
+
+		#region Constructors
+		public BankAccount(string accountNumber)
+		{
+			AccountNumber = accountNumber;
+		}
+
+		public BankAccount(string accountNumber, decimal balance):this(accountNumber)
+		{
+			Balance = balance;
+			//_withdrawCost = 0.10M;
+		}
+		#endregion
+	}
+}
--- Banking2A2/Program.cs
+++ Banking2A2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking2A2.Models.Domain;
+using System;
 
 namespace Banking2A2
 {
@@ -6,7 +7,24 @@ namespace Banking2A2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+			BankAccount myBA = new BankAccount("123-12312312-99");
+
+			Console.WriteLine($"Account {myBA.AccountNumber} created...");
+			Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+			//Console.WriteLine($"Withdrawcost is {BankAccount._withdrawCost} Euro");
+			BankAccount myOBA = new BankAccount("123-12312312-99",50);
+
+			Console.WriteLine($"Account {myOBA.AccountNumber} created...");
+			Console.WriteLine($"Balance is currently {myOBA.Balance} Euro");
+
+
+			myBA.Deposit(1000);
+			Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+			myBA.Withdraw(200);
+			Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+
+			Console.ReadKey();
 		}
 	}
 }
